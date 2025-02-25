8c29e097fc7e5ec4835825eb6f8e3365b48ecce2
legendMridul <123665281+legendMridul@users.noreply.github.com>
2025-02-25 17:27:20 +0000

    conversions

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,50 @@
+// let score =null
+// let score1= 0
+// let score3= true
+// let score4 =33
+// let score5 ="33abc"
+// let score6 =undefined
+
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score)
+// let valueInNumber1 = Number(score1)
+// let valueInNumber3 = Number(score3)
+// let valueInNumber4 = Number(score4)
+// let valueInNumber5 = Number(score5)
+// let valueInNumber6 = Number(score6)
+// console.log(typeof (valueInNumber));
+// console.log(typeof (valueInNumber1));
+// console.log(typeof (valueInNumber3));
+// console.log(typeof (valueInNumber4));
+// console.log(typeof (valueInNumber5));
+// console.log(typeof (valueInNumber6));
+// console.log(valueInNumber);
+// console.log(valueInNumber1);
+// console.log(valueInNumber3);
+// console.log(valueInNumber4);
+// console.log(valueInNumber5);
+// console.log(valueInNumber6);
+
+//"33"=>33
+//"33abc"=>NaN but type will be number
+//null=>0
+//undefined=>NaN
+//boolean=>1/0
+//string=>NaN
+ 
+  let isLoggendIn = 1
+  let booleanIsLoggedIn= Boolean(isLoggedIn)
+  console.log(booleanIsLoggedIn);
+
+
+  //1=>true
+  //-=>false
+  //" "=>false
+  //"mridul"=>true
+
+  let someNumber =33
+  let stringNumber =string(someNumber)
+  console.log(stringNumber)
+  console.log(typeof stringNumber)

f5e0e45824a05997db230be8bec74a8b9c98a4b4
legendMridul <123665281+legendMridul@users.noreply.github.com>
2025-02-25 16:38:38 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all JS code as new version
+
+// alert(6+6) //we are using nodejs,not browser
+
+console.log(3+5)//code readability should be high
+ console.log("Mridul")
+ let name ="Mridul"//string datatype
+ let age = 18//number
+ let isLoggedIn = false//boolean datatype
+let num;//undefiend
+let state= null;//null
+
+ //number => 2 to the pow 53
+ //bigint
+//string => ""
+//boolean => t/f
+//null => standalone value
+//undefined => 
+//symbol =>unique
+
+//object
+
+console.log(typeof "Mridul");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);

a36eb3ff4068506f5985a9a1206f32ccdb34f04a
legendMridul <123665281+legendMridul@users.noreply.github.com>
2025-02-26 05:08:08 +0000

    comparision

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,12 @@
+// console.log("2">1);
+// console.log("02">1);
+console.log(null>0);//null converted into 0  by comparision opetrator
+console.log(null ==0);
+console.log(null >= 0);
+
+console.log(undefined ==0)
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//=== also known as strict check it checks value as well as data type
+console.log( "0" === 0)

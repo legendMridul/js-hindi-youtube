12417ebd83674dc28b5ce82496586011cdbad3c5
legendMridul <123665281+legendMridul@users.noreply.github.com>
2025-02-26 05:07:55 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,27 @@
+// let value =3
+// let negValue =-value
+// console.log(negValue)
+// let str1 ="hello"
+// let str2=" mridul"
+// let str3 = str1 + str2
+// console.log(str3)
+// console.log("1" + 2);
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log(3+4*5%3)
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+ 
+//prefix n postfix
+let x=3;
+const y =x++;
+console.log(y);
+
+let a =3;
+const b = ++a;
+console.log(b);
+
+
